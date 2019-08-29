90b9aa70784e3e0940968f400042c6d8e526bef8
Alberto Fernandes <alberto4g@gmail.com>
2019-08-29 20:55:11 -0300

    Now show wich player won

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ var randomNumber2 = Math.floor((Math.random()*6) + 1);
 var randomImg1 = "./images/dice"+randomNumber1+".png";
 var randomImg2 = "./images/dice"+randomNumber2+".png";
 
+if(randomNumber1 == randomNumber2){
+    document.querySelector("h1").innerHTML = "DRAW!";
+} else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
 document.getElementsByClassName("img1")[0].setAttribute("src",randomImg1);
 document.getElementsByClassName("img2")[0].setAttribute("src",randomImg2);
